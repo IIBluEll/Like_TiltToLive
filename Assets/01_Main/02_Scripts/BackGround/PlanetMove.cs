949c1e5d6ea3faf23c56a96bf1ebabd5b3d9ad46
Leehyunmin <hmlee4135@naver.com>
2026-03-24 15:26:31 +0900

    배경화면 제작

---
+++ Assets/01_Main/02_Scripts/BackGround/PlanetMove.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class PlanetMove : MonoBehaviour
+{
+    private float _moveSpeed;
+    private float _leftCleanUpX;
+    private Action<PlanetMove> _onReturnToPool;
+
+    public void Init(float movespeed, float leftCleanUpX, Action<PlanetMove> onReturnToPool)
+    {
+        _moveSpeed = movespeed;
+        _leftCleanUpX = leftCleanUpX;
+        _onReturnToPool = onReturnToPool;
+
+        Animator tAnim = GetComponent<Animator>();
+        SpriteRenderer tSprite = GetComponent<SpriteRenderer>();
+
+        float tRandomNum = UnityEngine.Random.Range(1f,3f);
+
+        transform.localScale = Vector3.one * tRandomNum;
+        tAnim.speed = 1f - (0.15f * tRandomNum);
+
+        var tColor = tSprite.color;
+        tColor.a = Mathf.Lerp(0.3f, 0.7f, (tRandomNum - 1f) / 2f);
+        tSprite.color = tColor;
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.left * ( _moveSpeed * Time.deltaTime ));
+
+        if(transform.position. x < _leftCleanUpX )
+        {
+            ReturnToPool();
+        }
+    }
+
+    private void ReturnToPool()
+    {
+        transform.localScale = Vector3.one;
+        _onReturnToPool?.Invoke(this);
+        gameObject.SetActive(false);
+    }
+}
